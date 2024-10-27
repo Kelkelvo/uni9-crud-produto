6ce3b909e7e65e475759632d505be54eefc1d99d
Kelwin Williams Ferreira <143116048+Kelkelvo@users.noreply.github.com>
2024-10-27 14:53:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ echo 'Olá, mundo';
 ?>
 
 <div class="button-container">
-    <button onclick="window.location.href='produto-cadastrar.php';">Adicionar Produto</button>
-    <button onclick="window.location.href='produto-listar.php';">Listar Produtos</button>
+    <button onclick="window.location.href='adicionar_aluno.php';">Adicionar Alunos</button>
+    <button onclick="window.location.href='listar_alunos.php';">Listar Alunos</button>
 </div>
 
 </body>
